aecb64bd9e7d7cbb831ad5a2694176b7c6327464
phirom-02 <phiromkhim02@gmail.com>
2026-07-02 22:43:46 +0700

    initial commit

---
+++ bms-backend/src/main/java/com/firom/bms/BmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.firom.bms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BmsBackendApplication.class, args);
+    }
+
+}
---
+++ bms-gui/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.geekybyte.bmsgui {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.geekybyte.bmsgui to javafx.fxml;
+    exports com.geekybyte.bmsgui;
+}
